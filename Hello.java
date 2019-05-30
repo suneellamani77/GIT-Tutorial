5c0e4701d93995c0b98d172588c8250147e01bd7
Narendra <narendra.bodeddula@sgdigital.com>
2019-05-30 16:56:03 +0530

    changes on master-copy

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 class Simple{  
     public static void main(String args[]){  
 
-     System.out.println("Hello Java World..Modified by narendra");  
+     System.out.println("Hello Java World..Modified by narendra on master-copy branch");
+        System.out.println("created new branch");
 
     }  
 }  
