a89cc41ee21ffe31e5c25258e060f4626e5b39b7
NarendraBodeddula <narendra.bodeddula@gmail.com>
2019-05-30 16:42:35 +0530

    Update Hello.java
    modified by narendra

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java World..");  
+     System.out.println("Hello Java World..Modified by narendra");  
     }  
 }  
