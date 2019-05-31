6fb58dbb095f18c560f0af583bffdc5de6e2ce7d
Suneel <suneellamani77@gmail.com>
2019-05-31 15:10:34 +0530

    modified hello java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
 
+
      System.out.println("Hello Java World..Modified by narendra on master-copy branch");
         System.out.println("created new branch");
 
